51de835e2792f46fcd1834883cda7328d0e6a521
raphaelvittorio <raphaelvittoriomulyo@gmail.com>
2025-09-28 23:36:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_05/api/CatApiService.kt
@@ -0,0 +1,11 @@
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CatApiService {
+    @GET("images/search")
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("size") format: String
+    ) : Call<String>
+}
